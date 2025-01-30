2d06d82892269cc39cf93388cee0ac6e2d4b529c
Qwerty22113 <156567922+Qwerty22113@users.noreply.github.com>
2025-01-30 20:30:31 +0800

    Update source.js

--- source.js
+++ source.js
@@ -1,69 +1,68 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Your existing JavaScript code here
-  const titleElement = document.querySelector(".title");
-  const buttonsContainer = document.querySelector(".buttons");
-  const yesButton = document.querySelector(".btn--yes");
-  const noButton = document.querySelector(".btn--no");
-  const catImg = document.querySelector(".cat-img");
+"use strict";
 
-  const MAX_IMAGES = 5;
+const titleElement = document.querySelector(".title");
+const buttonsContainer = document.querySelector(".buttons");
+const yesButton = document.querySelector(".btn--yes");
+const noButton = document.querySelector(".btn--no");
+const catImg = document.querySelector(".cat-img");
 
-  let play = true;
-  let noCount = 0;
+const MAX_IMAGES = 5;
 
-  // Debugging: Check if elements are found
-  console.log("Title element:", titleElement);
-  console.log("Yes button:", yesButton);
-  console.log("No button:", noButton);
+let play = true;
+let noCount = 0;
 
-  yesButton.addEventListener("click", handleYesClick);
+// Debugging: Check if elements are found
+console.log("Title element:", titleElement);
+console.log("Yes button:", yesButton);
+console.log("No button:", noButton);
 
-  noButton.addEventListener("click", function () {
-    if (play) {
-      noCount++;
-      const imageIndex = Math.min(noCount, MAX_IMAGES);
-      changeImage(imageIndex);
-      resizeYesButton();
-      updateNoButtonText();
-      if (noCount === MAX_IMAGES) {
-        play = false;
-      }
+yesButton.addEventListener("click", handleYesClick);
+
+noButton.addEventListener("click", function () {
+  if (play) {
+    noCount++;
+    const imageIndex = Math.min(noCount, MAX_IMAGES);
+    changeImage(imageIndex);
+    resizeYesButton();
+    updateNoButtonText();
+    if (noCount === MAX_IMAGES) {
+      play = false;
     }
-  });
-
-  function handleYesClick() {
-    titleElement.innerHTML = "Yayyy!! :3";
-    buttonsContainer.classList.add("hidden");
-    changeImage("yes");
-  }
-
-  function resizeYesButton() {
-    const computedStyle = window.getComputedStyle(yesButton);
-    const fontSize = parseFloat(computedStyle.getPropertyValue("font-size"));
-    const newFontSize = fontSize * 1.6;
-
-    yesButton.style.fontSize = `${newFontSize}px`;
-  }
-
-  function generateMessage(noCount) {
-    const messages = [
-      "No",
-      "Are you sure?",
-      "Pookie please",
-      "Don't do this to me :(",
-      "You're breaking my heart",
-      "I'm gonna cry...",
-    ];
-
-    const messageIndex = Math.min(noCount, messages.length - 1);
-    return messages[messageIndex];
-  }
-
-  function changeImage(image) {
-    catImg.src = `img/cat-${image}.jpg`;
-  }
-
-  function updateNoButtonText() {
-    noButton.innerHTML = generateMessage(noCount);
   }
 });
+
+function handleYesClick() {
+  titleElement.innerHTML = "Yayyy!! :3";
+  buttonsContainer.classList.add("hidden");
+  changeImage("yes");
+}
+
+function resizeYesButton() {
+  const computedStyle = window.getComputedStyle(yesButton);
+  const fontSize = parseFloat(computedStyle.getPropertyValue("font-size"));
+  const newFontSize = fontSize * 1.6;
+
+  yesButton.style.fontSize = `${newFontSize}px`;
+}
+
+function generateMessage(noCount) {
+  const messages = [
+    "No",
+    "Are you sure?",
+    "Pookie please",
+    "Don't do this to me :(",
+    "You're breaking my heart",
+    "I'm gonna cry...",
+  ];
+
+  const messageIndex = Math.min(noCount, messages.length - 1);
+  return messages[messageIndex];
+}
+
+function changeImage(image) {
+  catImg.src = `img/cat-${image}.jpg`;
+}
+
+function updateNoButtonText() {
+  noButton.innerHTML = generateMessage(noCount);
+}
